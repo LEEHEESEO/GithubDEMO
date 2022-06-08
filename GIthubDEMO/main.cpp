5ebc6b22b7026c5b874afba37ee6d8c7eb7d27b9
LEEHEESEO <ransom722@naver.com>
2022-06-08 19:50:19 +0900

    Initial Commit

---
+++ GIthubDEMO/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GIthubDEMO
+//
+//  Created by 이희서 on 2022/06/08.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
